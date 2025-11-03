82d19a72f64675fbadaae1854fd2b85b14f8a2e4
Technovahub Career <technovahubcareer@gmail.com>
2025-11-03 19:10:27 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import adsImg from "../assets/oldad.jpg";
+import adsImg from "../assets/newads.jpg";
 
 const Home = () => {
   return (
